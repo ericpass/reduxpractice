3d6c77a6d25e48b2b8e6b94a0b0bb0afa57960c7
Eric Pass <eric.pass@slalom.com>
2020-02-11 16:11:47 -0500

    test3

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import Root from './containers/Root';
 
-const foo = 'bar';
+const bar = 'baz';
 
 render(<Root />, document.getElementById('root'));
